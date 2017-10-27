86f96a2b3b39e6f8b2c9c007f72df2655e74b157
zhangshuming <zhangshuming@zhangshuming.com>
2017-10-27 11:40:51 +0800

    增加6-30天数据

--- collection/src/api/api.js
+++ collection/src/api/api.js
@@ -4,19 +4,19 @@ let base = '';
 
 export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
 
-export const getUserList = params => { return axios.get(`${base}/user/list`, { params: params }); };
+// export const getUserList = params => { return axios.get(`${base}/user/list`, { params: params }); };
 
 export const getUserListPage = params => { return axios.get(`${base}/user/listpage`, { params: params }); }; 
 
 export const getUserListPage1 = params => { return axios.get(`${base}/user1/listpage`, { params: params }); };
 
-export const removeUser = params => { return axios.get(`${base}/user/remove`, { params: params }); };
+// export const removeUser = params => { return axios.get(`${base}/user/remove`, { params: params }); };
 
-export const batchRemoveUser = params => { return axios.get(`${base}/user/batchremove`, { params: params }); };
+// export const batchRemoveUser = params => { return axios.get(`${base}/user/batchremove`, { params: params }); };
 
-export const editUser = params => { return axios.get(`${base}/user/edit`, { params: params }); };
+// export const editUser = params => { return axios.get(`${base}/user/edit`, { params: params }); };
 
-export const addUser = params => { return axios.get(`${base}/user/add`, { params: params }); };
+// export const addUser = params => { return axios.get(`${base}/user/add`, { params: params }); };
 
 export const nav_view = params => { return axios.get(`${base}/nav_view`, { params: params }); };
 
