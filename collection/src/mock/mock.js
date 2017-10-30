5662181663a0555bf1b24b21c62f0901b7b44457
zhangshuming <zhangshuming@zhangshuming.com>
2017-10-30 13:00:35 +0800

    催收历史页面数据绑定

--- collection/src/mock/mock.js
+++ collection/src/mock/mock.js
@@ -6,6 +6,7 @@ import { LoginUsers, userList } from './data/user';
 import { station, userstation, supervisor,history } from './data/monitor';
 import { NavView } from './data/navview';
 import { TabView } from './data/tabView';
+import {TabMessage} from './data/tabMessage'
 //同样以LoginUsers, Users 的方式来接收，from的url
 let _Users = userList[0].data;
 let _Users1 = Users1;
@@ -119,6 +120,14 @@ export default {
  		mock.onGet('/tab_view').reply(200, {
         msg:TabView
    });
+   //催收历史
+   
+   	mock.onGet('/tab_message').reply(200, {
+        msg:TabMessage
+   });
+   
+   
+   
     // //获取用户列表
     // mock.onGet('/user/list').reply(config => {
     //    //config.params放的是用户输入的name,params是user.vue中传递的
