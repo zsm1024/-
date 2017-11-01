5682206acff24e01f5950850871c81877b4228b4
zhangshuming <zhangshuming@zhangshuming.com>
2017-11-01 19:42:55 +0800

    界面友好性调整

--- collection/src/components/home/Index.vue
+++ collection/src/components/home/Index.vue
@@ -36,16 +36,14 @@ export default {
     left: 0;
     top: 0;
     bottom: 0;
-    min-width:60px;
 	color:#000;
-    width:5rem;
+
 }
 .navopen {
     position: absolute;
     left: 0;
     top: 0;
     bottom: 0;
-    width:15rem;
 
 }
 .pane {
@@ -66,28 +64,6 @@ export default {
     overflow-x: hidden;
     overflow-y: hidden;
 }
-/*.el-menu-vertical-demo:not(.el-menu--collapse) {
-    width: 235px;
-    min-height: 400px;
-}
 
-.logo {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: white;
-  height: 3.5rem;
-  border-bottom: solid 0.01rem #48576a;
-}
-.logo a{
-  color:#fff
-}
-i {
-  margin-right: 0.5rem;
-}
-
-.left-nav {
-  padding: 0.3rem;
-}*/
 </style>
 
