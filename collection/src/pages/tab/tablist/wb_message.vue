4196fc4d0b3cd5efe18a9e0784e8147658d68787
zhangshuming <zhangshuming@zhangshuming.com>
2017-11-13 08:45:08 +0800

    数据修改

--- collection/src/pages/tab/tablist/wb_message.vue
+++ collection/src/pages/tab/tablist/wb_message.vue
@@ -3,28 +3,28 @@
   <el-collapse-item name="1" title="车辆信息">
 	  <el-form  inline class="expand">
 		  	<el-form-item  v-for=" a in carInfo" :key="a.title" :lable="a.title" style="margin:0">
-				<label style="min-width:400px;display:inline-block;"><span class="title">{{a.title}}:</span><span class="lable">{{a.field}}</span></label>	
+				<label style="min-width:400px;display:inline-block;"><span class="titles">{{a.title}}:</span><span class="lable">{{a.field}}</span></label>	
 			</el-form-item>
 	  </el-form>			
   </el-collapse-item>	
   <el-collapse-item name="2" title="客户信息">
 	  <el-form  inline class="expand">
 		  	<el-form-item  v-for=" a in userInfo" :key="a.title" :lable="a.title" style="margin:0">
-				<label style="min-width:400px;display:inline-block"><span class="title">{{a.title}}:</span><span class="lable">{{a.field}}</span></label>	
+				<label style="min-width:400px;display:inline-block"><span class="titles">{{a.title}}:</span><span class="lable">{{a.field}}</span></label>	
 			</el-form-item>
 	  </el-form>			
   </el-collapse-item>
   <el-collapse-item name="3" title="送修人信息">
 	  <el-form  inline class="expand">
 		  	<el-form-item  v-for=" a in repairPerson" :key="a.title" :lable="a.title" style="margin:0">
-				<label style="min-width:400px;display:inline-block"><span class="title">{{a.title}}:</span><span class="lable">{{a.field}}</span></label>	
+				<label style="min-width:400px;display:inline-block"><span class="titles">{{a.title}}:</span><span class="lable">{{a.field}}</span></label>	
 			</el-form-item>
 	  </el-form>			
   </el-collapse-item>
   <el-collapse-item name="4" title="维保记录">
 	  <el-form  inline class="expand">
 		  	<el-form-item  v-for=" a in repairInfo" :key="a.title" :lable="a.title" style="margin:0">
-				<label style="min-width:400px;display:inline-block"><span class="title">{{a.title}}:</span><span class="lable">{{a.field}}</span></label>	
+				<label style="min-width:400px;display:inline-block"><span class="titles">{{a.title}}:</span><span class="lable">{{a.field}}</span></label>	
 			</el-form-item>
 	  </el-form>			
   </el-collapse-item>
@@ -52,7 +52,6 @@ export default {
                 let data = res.data.data[0]
 				 this.items=data;
 				 this.carInfo=data.carInfo;
-				 console.log(data.carInfo)
 				 this.repairPerson=data.repairPerson;
 				 this.repairInfo=data.repairInfo;
 				 this.userInfo=data.userInfo;			     
@@ -67,6 +66,6 @@ export default {
 
 <style>
 	.lable{color: #99a9bf;margin:0 10px;}
-	.title{display:inline-block;width:90px;text-align:right}
+	.titles{display:inline-block;width:90px;text-align:right}
 		
 </style>
