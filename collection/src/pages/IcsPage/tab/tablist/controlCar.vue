db7ac4a37109c0a5753ee210efc6d6055ed5f9af
zhangshuming <zhangshuming@gwmfc.com>
2018-11-01 16:27:03 +0800

    控制车辆信息标签中格式化提车日期

--- collection/src/pages/IcsPage/tab/tablist/controlCar.vue
+++ collection/src/pages/IcsPage/tab/tablist/controlCar.vue
@@ -29,21 +29,9 @@ export default {
         id:this.$route.params.id,
       };
       getOsControlVehicleD(para).then(res=> {
-          this.carlist =res.data.result;
-        //  var date = new Date();
-				// var seperator1 = "-";
-				// var month = date.getMonth() + 1;
-				// var strDate = date.getDate();
-				// if (month >= 1 && month <= 9) {
-				// 	month = "0" + month;
-				// }
-				// if (strDate >= 0 && strDate <= 9) {
-				// 	strDate = "0" + strDate;
-				// }
-				// var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate;										
+          this.carlist =res.data.result;									
          this.carlist.forEach(el => {
-           element.pickUpDate=element.pickUpDate ? moment(element.pickUpDate).format('YYYY-MM-DD'):""
-          // el.pickUpDate=currentdate;
+           el.pickUpDate=el.pickUpDate ? Moment(el.pickUpDate).format('YYYY-MM-DD'):""
          });
       })
     }
