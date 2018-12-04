34bbe1970ca35f590754b9008dba30e3dd143213
zhangshuming <zhangshuming@gwmfc.com>
2018-12-04 15:04:14 +0800

    删除无用的东西

--- config/prod.env.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-BASE_API: '"http://10.50.132.72:18081"',
- //BASE_API: '"http://10.50.132.72:18082"',//测试地址
-  //  BASE_API: '"http://192.168.2.111:8081"'
- //BASE_API: '"http://10.5.1.50:10001"',//生产地址
-}
